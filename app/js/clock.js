46064212bab8b8fd951208099f7bc45582b330f4
Rudolf Hersen <rhersen@gmail.com>
2014-10-16 20:31:05 +0200

    3840qm

--- app/js/clock.js
+++ app/js/clock.js
@@ -9,25 +9,17 @@ var Clock = React.createClass({
 
    render: function () {
       return React.DOM.svg({
-            viewBox: '-1.5 -1.5 3 3'
+            viewBox: '-1.5 -1.5 3 3',
+            onMouseMove: function (ev) {
+               console.log(ev.clientX);
+            }
          },
-         React.DOM.circle({
-            r: 1,
-            fill: 'ivory'}),
-         Hand({
-            length: 0.7,
-            width: 0.1,
-            angle: this.getAngle(12 * 60 * 60000)
-         }),
-         Hand({
-            length: 0.9,
-            width: 0.05,
-            angle: this.getAngle(60 * 60000)
-         }),
-         Hand({
-            length: 0.9,
-            width: 0.01,
-            angle: this.getAngle(60000)
+         React.DOM.line({
+            y2: -0.9,
+            transform: 'rotate(' + this.getAngle(60000) + ')',
+            strokeLinecap: 'square',
+            strokeWidth: 0.003,
+            stroke: 'white'
          })
       );
    }
